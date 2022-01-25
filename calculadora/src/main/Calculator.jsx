c7b615434823a7dd511653eecf9ca1e3161756c3
Josimar Ferreira <josimarferreira922@yahoo.com.br>
2022-01-24 22:37:42 -0300

    Criando componente Calculator

---
+++ calculadora/src/main/Calculator.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import './Calculator.css'
+
+export default class Calculator extends Component{
+    render(){
+        return(
+            // Em JSX ao invés da propriedade class do HTML , usamos className
+            <div className='calculator'>
+
+            </div>
+        )
+    } 
+    
+}
